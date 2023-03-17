a2e41d0583638822768d4c07f588f713a844bd90
ruben <rubensinuk@gmail.com>
2023-03-17 18:28:43 +0530

    added the navbar and homepage starting styles

---
+++ ems/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RootLayout from "./pages/Root";
+import HomePage from './pages/HomePage';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout/>,
+    children:[
+      {
+        index:true,
+        element: <HomePage/>,
+      }
+    ]
+  }
+])
+function App() {
+  return <RouterProvider router={router}/>
+}
+
+export default App;
---
+++ ems/src/pages/HomePage.js
@@ -0,0 +1,9 @@
+
+const HomePage = (props) => {
+    return (
+      <>
+        <h1>Hi Again</h1>
+      </>
+    );
+}
+export default HomePage;
